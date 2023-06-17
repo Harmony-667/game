5087f4b401b98ff5d06249dd556364c274fc9474
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2023-06-17 19:28:20 +0200

    Fixed doors
    Fixed yanille doors
    Changed redkey door down to the dungeon

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/yanille/red_key_door.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/yanille/red_key_door.plugin.kts
@@ -4,7 +4,7 @@ package gg.rsmod.plugins.content.areas.yanille
  * @author Alycia <https://github.com/alycii>
  */
 
-on_obj_option(obj = Objs.DOOR_1533, option = "open") {
+on_obj_option(obj = Objs.DOOR_2559, option = "open") {
     if(!player.inventory.contains(Items.KEY_1543)) {
         player.message("The door is locked.")
         return@on_obj_option
@@ -23,16 +23,16 @@ on_obj_option(obj = Objs.STAIRCASE_32270, option = "Climb-Up") {
 
 
 fun handleDoor(player: Player) {
-    val closedDoor = DynamicObject(id = 1533, type = 0, rot = 3, tile = Tile(x = 2603, z = 3082))
-    val door = DynamicObject(id = 1533, type = 0, rot = if (player.tile.z == 3080) 2 else 1, tile = Tile(x = 3115, z = 3082))
+    val closedDoor = DynamicObject(id = 2559, type = 0, rot = 3, tile = Tile(x = 2601, z = 9482))
+    val door = DynamicObject(id = 2559, type = 0, rot = if (player.tile.z == 9481) 2 else 2, tile = Tile(x = 2601, z = 9482))
     player.lock = LockState.DELAY_ACTIONS
     world.remove(closedDoor)
     player.playSound(Sfx.DOOR_OPEN)
     world.spawn(door)
 
     player.queue {
-        val x = 2603
-        val z = if (player.tile.z == 3081) 3082 else 3081
+        val x = 2601
+        val z = if (player.tile.z == 9481) 9482 else 9481
         player.walkTo(tile = Tile(x = x, z = z), detectCollision = false)
         wait(3)
         world.remove(door)
