ce8792c909fd0140166184edcb3fc5148e59da95
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2023-06-17 18:40:42 +0200

    Fix on redkey door

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/yanille/red_key_door.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/yanille/red_key_door.plugin.kts
@@ -14,17 +14,17 @@ on_obj_option(obj = Objs.DOOR_1533, option = "open") {
 }
 
 on_obj_option(obj = Objs.STAIRCASE_32271, option = "Climb-Down") {
-    player.handleLadder(underground = true)
+    player.moveTo(2602, 9479)
 }
 
-on_obj_option(obj = Objs.LADDER_29358, option = "Climb-Down") {
-    player.handleLadder()
+on_obj_option(obj = Objs.STAIRCASE_32270, option = "Climb-Up") {
+    player.moveTo(2606, 3079)
 }
 
 
 fun handleDoor(player: Player) {
     val closedDoor = DynamicObject(id = 1533, type = 0, rot = 3, tile = Tile(x = 2603, z = 3082))
-    val door = DynamicObject(id = 1533, type = 0, rot = if (player.tile.z == 3080) 1 else 3, tile = Tile(x = 3115, z = 3082))
+    val door = DynamicObject(id = 1533, type = 0, rot = if (player.tile.z == 3080) 2 else 1, tile = Tile(x = 3115, z = 3082))
     player.lock = LockState.DELAY_ACTIONS
     world.remove(closedDoor)
     player.playSound(Sfx.DOOR_OPEN)
