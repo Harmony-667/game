02ad68f745378038c24a6ac61026b47020ed10bc
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2023-06-25 18:03:28 +0200

    Added White Wolf level 38 config

---
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/npcs/definitions/animals/white_wolf_lvl_38.plugin.kts
@@ -0,0 +1,63 @@
+package gg.rsmod.plugins.content.npcs.definitions.animals
+
+import gg.rsmod.game.model.combat.SlayerAssignment
+import gg.rsmod.plugins.content.drops.DropTableFactory
+
+/**
+ * @author Alycia <https://github.com/alycii>
+ */
+val ids = intArrayOf(Npcs.WHITE_WOLF_97)
+
+val table = DropTableFactory
+val whiteWolf = table.build {
+    guaranteed {
+        obj(Items.WOLF_BONES, quantity = 1)
+    }
+    main {
+        total(512)
+        obj(Items.STEEL_PICKAXE, quantity = 1, slots = 2)
+        obj(Items.MITHRIL_PICKAXE, quantity = 1, slots = 1)
+        nothing(slots = 256)
+    }
+}
+
+table.register(whiteWolf, *ids)
+
+on_npc_pre_death(*ids) {
+    val p = npc.damageMap.getMostDamage()!! as Player
+    p.playSound(Sfx.WOLF_DEATH)
+}
+
+on_npc_death(*ids) {
+    table.getDrop(world, npc.damageMap.getMostDamage()!! as Player, npc.id, npc.tile)
+}
+
+ids.forEach {
+    set_combat_def(it) {
+        configs {
+            attackSpeed = 4
+            respawnDelay = 90
+        }
+        stats {
+            hitpoints = 440
+            attack = 30
+            strength = 31
+            defence = 32
+            magic = 1
+            ranged = 1
+        }
+        anims {
+            attack = 6579
+            death = 6576
+            block = 6578
+        }
+        aggro {
+            radius = 4
+        }
+        slayer {
+            assignment = SlayerAssignment.WOLF
+            level = 1
+            experience = 44.0
+        }
+    }
+}
