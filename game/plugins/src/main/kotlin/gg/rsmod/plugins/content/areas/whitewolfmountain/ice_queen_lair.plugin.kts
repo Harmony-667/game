d5d6f92c4a0cbd8069c86cf61e56f09151621550
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2023-06-25 19:18:34 +0200

    Add multi combat to ice queen lair

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/whitewolfmountain/ice_queen_lair.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/whitewolfmountain/ice_queen_lair.plugin.kts
@@ -15,3 +15,5 @@ on_obj_option(obj = Objs.LADDER_33184, option = "climb-up") { //handles the ladd
         player.moveTo(Tile(2844, 3516, 0)) //sets the x, z, height for player destination
     }
 }
+private val ICE_QUEEN_LAIR_REGIONS = intArrayOf(11419, 11418) //Multi combat
+ICE_QUEEN_LAIR_REGIONS.forEach { set_multi_combat_region(region = it) }
