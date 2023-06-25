13d86315e1233028bac157e0b0286b238f6a3bed
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2023-06-25 16:02:54 +0200

    Added Staff zone

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/cmd/commands.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/cmd/commands.plugin.kts
@@ -202,6 +202,10 @@ on_command("home", Privilege.ADMIN_POWER) {
     val home = world.gameContext.home
     player.moveTo(home)
 }
+on_command("staffzone", Privilege.MOD_POWER) {
+    player.moveTo(2935, 4695)
+    player.message("You teleported to the Staff Zone")
+}
 
 on_command("changepass") {
     val args = player.getCommandArgs()
