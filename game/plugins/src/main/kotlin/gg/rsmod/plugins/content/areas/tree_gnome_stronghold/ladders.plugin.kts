fbc5c954de39f9a30eeb8dfe46f76139a4c980d4
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2023-07-07 15:16:11 +0200

    Disabled ladders
    Need to fix it later with legends guild

---
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/tree_gnome_stronghold/ladders.plugin.kts
@@ -0,0 +1,53 @@
+package gg.rsmod.plugins.content.areas.tree_gnome_stronghold
+/**
+ * @author Eikenb00m <https://github.com/eikenb00m>
+ */
+/*
+on_obj_option(obj = Objs.LADDER_1746, option = "Climb-Down", lineOfSightDistance = -1) {
+    if (pawn.tile.x == 2437 && pawn.tile.z == 3463 && pawn.tile.height == 1) {
+        player.handleLadder(x = 2437, z = 3463, height = 0)
+    }
+    else if (pawn.tile.x == 2443 && pawn.tile.z == 3462 && pawn.tile.height == 1) {
+        player.handleLadder(x = 2443, z = 3462, height = 0)
+    }
+    else if (pawn.tile.x == 2449 && pawn.tile.z == 3463 && pawn.tile.height == 1) {
+        player.handleLadder(x = 2449, z = 3463, height = 0)
+    }
+    else if (pawn.tile.x == 2477 && pawn.tile.z == 3463 && pawn.tile.height == 1) {
+        player.handleLadder(x = 2477, z = 3463, height = 0)
+    }
+    else if (pawn.tile.x == 2423 && pawn.tile.z == 3441 && pawn.tile.height == 1) {
+        player.handleLadder(x = 2423, z = 3441, height = 0)
+    }
+    else if (pawn.tile.x == 2408 && pawn.tile.z == 3436 && pawn.tile.height == 1) {
+        player.handleLadder(x = 2408, z = 3436, height = 0)
+    }
+    else {
+        player.message("This Ladder is not Working! Post it on Discord!")
+    }
+}
+on_obj_option(obj = Objs.LADDER_1747, option = "Climb-Up", lineOfSightDistance = 1) {
+    if (pawn.tile.x == 2437 && pawn.tile.z == 3463 && pawn.tile.height == 0) {
+        player.handleLadder(x = 2437, z = 3463, height = 1)
+    }
+    else if (pawn.tile.x == 2443 && pawn.tile.z == 3462 && pawn.tile.height == 0) {
+        player.handleLadder(x = 2443, z = 3462, height = 1)
+    }
+    else if (pawn.tile.x == 2449 && pawn.tile.z == 3463 && pawn.tile.height == 0) {
+        player.handleLadder(x = 2449, z = 3463, height = 1)
+    }
+    else if (pawn.tile.x == 2477 && pawn.tile.z == 3463 && pawn.tile.height == 0) {
+        player.handleLadder(x = 2477, z = 3463, height = 1)
+    }
+    else if (pawn.tile.x == 2423 && pawn.tile.z == 3441 && pawn.tile.height == 0) {
+        player.handleLadder(x = 2423, z = 3441, height = 1)
+    }
+    else if (pawn.tile.x == 2408 && pawn.tile.z == 3436 && pawn.tile.height == 0) {
+        player.handleLadder(x = 2408, z = 3436, height = 1)
+    }
+    else {
+        player.message("This Ladder is not Working! Post it on Discord!")
+    }
+}
+*/
+
