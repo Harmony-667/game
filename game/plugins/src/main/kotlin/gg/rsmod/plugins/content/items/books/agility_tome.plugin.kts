9112f3dd5244676f6fd1782568535a5286f3f7b2
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2023-07-10 23:21:42 +0200

    Added Agility Tomes

---
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/items/books/agility_tome.plugin.kts
@@ -0,0 +1,46 @@
+package gg.rsmod.plugins.content.items.books
+
+/**
+ * @author Eikenb00m <https://github.com/Eikenb00m>
+ */
+val multiplier = 4
+
+on_item_option(item = Items.AGILITY_TOME, option = "read") {
+    if (player.inventory.remove(Items.AGILITY_TOME).hasSucceeded()) {
+        val experience = world.randomDouble(1100.0..1650.0) * multiplier
+        val experiencefmt = experience //add format later!
+        player.addXp(skill = Skills.AGILITY, xp = experiencefmt, modifiers = false)
+        player.queue {
+            doubleMessageBox(
+                "Your have read the book, and gain some knowledge!",
+                "You have been awarded ${experiencefmt} Agility experience!"
+            )
+        }
+    }
+}
+on_item_option(item = Items.AGILITY_TOME_7783, option = "read") {
+    if (player.inventory.remove(Items.AGILITY_TOME).hasSucceeded()) {
+        val experience = world.randomDouble(2035.0..3025.0) * multiplier
+        val experiencefmt = experience //add format later!
+        player.addXp(skill = Skills.AGILITY, xp = experiencefmt, modifiers = false)
+        player.queue {
+            doubleMessageBox(
+                "Your have read the book, and gain some knowledge!",
+                "You have been awarded ${experiencefmt} Agility experience!"
+            )
+        }
+    }
+}
+on_item_option(item = Items.AGILITY_TOME_7784, option = "read") {
+    if (player.inventory.remove(Items.AGILITY_TOME).hasSucceeded()) {
+        val experience = world.randomDouble(4015.0..5005.0) * multiplier
+        val experiencefmt = experience //add format later!
+        player.addXp(skill = Skills.AGILITY, xp = experiencefmt, modifiers = false)
+        player.queue {
+            doubleMessageBox(
+                "Your have read the book, and gain some knowledge!",
+                "You have been awarded ${experiencefmt} Agility experience!"
+            )
+        }
+    }
+}
