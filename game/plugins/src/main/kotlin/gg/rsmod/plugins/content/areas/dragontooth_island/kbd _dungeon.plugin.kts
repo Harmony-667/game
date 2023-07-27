a876a31f4322b8078f483cad501fb4c4870191fa
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2023-07-27 18:51:37 +0200

    Moved objects to Dragontooth island, add multi combat

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/dragontooth_island/kbd _dungeon.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/dragontooth_island/kbd _dungeon.plugin.kts
@@ -3,12 +3,13 @@ package gg.rsmod.plugins.content.areas.dragontooth_island
 /**
  * @author Eikenb00m <https://github.com/eikenb00m>
  */
-//Entering KBD Dungeon
-world.spawn(DynamicObject(id = Objs.CAVE_ENTRANCE, type = 10, rot = 2, tile = Tile(3801, 3529, 0)))//spawn Cave entrance
-
-on_obj_option(obj = Objs.CAVE_ENTRANCE, option = "enter") {
-    player.moveTo(3017, 10248)
-}
 on_obj_option(obj = Objs.LADDER_32015, option = "climb-up") {
     player.moveTo(3803, 3533)
 }
+world.spawn(DynamicObject(id = Objs.PILE_OF_SKULLS, type = 10, rot = 0, tile = Tile(3069, 10256, 0)))
+on_obj_option(obj = Objs.LEVER_1816, option = "pull") {
+    player.moveTo(2273, 4681)
+}
+
+private val KBD_DUNGEON_REGIONS = intArrayOf(12192) //Multi combat
+KBD_DUNGEON_REGIONS.forEach { set_multi_combat_region(region = it) }
