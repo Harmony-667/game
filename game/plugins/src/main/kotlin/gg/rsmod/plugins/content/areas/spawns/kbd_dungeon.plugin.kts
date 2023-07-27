a67e40b83f2c6ffe12cfded72543b8ac0e37835d
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2023-07-27 18:52:01 +0200

    Add Brutal Dragon

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/spawns/kbd_dungeon.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/spawns/kbd_dungeon.plugin.kts
@@ -12,3 +12,4 @@ spawn_npc(npc = Npcs.BLACK_DRAGON, x = 3040, z = 10255, walkRadius = 5, directio
 spawn_npc(npc = Npcs.BLACK_DRAGON, x = 3039, z = 10247, walkRadius = 5, direction = Direction.SOUTH_EAST)
 spawn_npc(npc = Npcs.BLACK_DRAGON, x = 3050, z = 10256, walkRadius = 5, direction = Direction.SOUTH_EAST)
 spawn_npc(npc = Npcs.KING_BLACK_DRAGON, x = 3053, z = 10272, walkRadius = 5, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.BRUTAL_GREEN_DRAGON, x = 3066, z = 10254, walkRadius = 5, direction = Direction.SOUTH)
