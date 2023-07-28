8dd19b6f2129c073081d71b4416d4eb4cfa9c417
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2023-07-28 18:21:39 +0200

    Fix for same ladder ids

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/dragontooth_island/kbd _dungeon.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/dragontooth_island/kbd _dungeon.plugin.kts
@@ -4,8 +4,21 @@ package gg.rsmod.plugins.content.areas.dragontooth_island
  * @author Eikenb00m <https://github.com/eikenb00m>
  */
 on_obj_option(obj = Objs.LADDER_32015, option = "climb-up") {
-    player.moveTo(3803, 3533)
+    when(player.tile.regionId) {
+        12181 -> { // Wizards Tower
+            player.handleLadder(x = 3104, z = 3161)
+        }
+        11673 -> { // Taverly Dungeon
+            player.handleLadder(x = 2884, z = 3398)
+        }
+        11417 -> { // Waterobilisk Island
+            //player.handleLadder(x = 2842, z = 3423)
+            player.handleLadder(x = 2884, z = 3398)
+        }
+        else -> player.handleLadder(x = 3803, z = 3533) //KBD dungeon
+    }
 }
+
 world.spawn(DynamicObject(id = Objs.PILE_OF_SKULLS, type = 10, rot = 0, tile = Tile(3069, 10256, 0)))
 on_obj_option(obj = Objs.LEVER_1816, option = "pull") {
     player.moveTo(2273, 4681)
