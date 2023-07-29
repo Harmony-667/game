2619c34ce21a21b2afe046ff9ef8a52bb55e9ff0
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2023-07-29 14:45:07 +0200

    Update Assignments

--- game/src/main/kotlin/gg/rsmod/game/model/combat/SlayerAssignment.kt
+++ game/src/main/kotlin/gg/rsmod/game/model/combat/SlayerAssignment.kt
@@ -1,46 +1,16 @@
 package gg.rsmod.game.model.combat
-
 /**
  * @author Alycia <https://github.com/alycii>
  */
 enum class SlayerAssignment(val identifier: String) {
-    ABERRANT_SPECTRE("Aberrant spectres"),
-    ABYSSAL_DEMON("Abyssal Demons"),
-    BIRD("Birds"),
-    BANSHEE("Banshees"),
-    BEAR("Bears"),
-    BLOODVELD("Bloodvelds"),
-    CAVE_BUG("Cave Bugs"),
-    CAVE_SLIME("Cave Slimes"),
     CAVE_CRAWLER("Cave Crawlers"),
     CRAWLING_HAND("Crawling Hands"),
-    COCKATRICE("Cockatrice"),
-    DESERT_LIZARD("Desert Lizards"),
-    DOG("Dogs"),
-    DWARF("Dwarves"),
-    GARGOYLE("Gargoyles"),
-    GREEN_DRAGON("Green Dragons"),
-    GHOST("Ghosts"),
-    GHOUL("Ghouls"),
-    GOBLIN("Goblins"),
-    HILL_GIANT("Hill Giants"),
-    ICEFIEND("Icefiends"),
-    INFERNAL_MAGE("Infernal mages"),
-    LESSER_DEMON("Lesser Demons"),
-    MINOTAUR("Minotaurs"),
-    MOSS_GIANT("Moss Giants"),
-    MONKEY("Monkeys"),
-    NECHRYAEL("Nechryaels"),
+    JELLY("Jellies"),
+    DEATHSPAWN("Death Spawns"),
+    WILDDOG("Wild Dogs"),
+    TORTURED_SOUL("Tortured Souls"),
+    WATERFIEND("Waterfiends"),
+    MOURNER("Mourners"),
     PYREFIEND("Pyrefiends"),
-    ROCK_SLUG("Rockslugs"),
-    SCORPION("Scorpions"),
-    SKELETON("Skeletons"),
-    SPIDER("Spiders"),
-    WOLF("Wolves"),
-    HELLHOUNDS("Hellhounds"),
-    ZOMBIE("Zombies"),
-    BAT("Bats"),
-    COW("Cows"),
-    ICE_WARRIOR("Ice Warriors"),
-    ICE_GIANT("Ice Giants");
+    CAVE_SLIME("Cave Slimes");
 }
