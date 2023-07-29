27ead7b44d8270a86a773bfc6471431cf5d2bd6e
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2023-07-29 10:32:01 +0200

    Fixed some premium stuff

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/magic/teleports/home_teleport_spells.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/magic/teleports/home_teleport_spells.plugin.kts
@@ -37,7 +37,7 @@ HomeTeleport.values.forEach { teleport ->
             return@on_magic_spell_button
         }
 
-        if (HOME_TELEPORT_TIMER_ENABLED && player.timers.has(HOME_TELEPORT_TIMER) && player.privilege.id != 2) {
+        if (HOME_TELEPORT_TIMER_ENABLED && player.timers.has(HOME_TELEPORT_TIMER) && player.privilege.id != 3) {
             val minutes = player.timers.getMinutesLeft(HOME_TELEPORT_TIMER)
 
             if (minutes != null) {
