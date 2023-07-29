2159ec5e5aa2c6a73846288ee06328a8b5f351be
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2023-07-29 10:45:24 +0200

    Add White wolf dungeon spawns

---
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/spawns/whitewolftunnel.dungeon.plugin.kts
@@ -0,0 +1,18 @@
+package gg.rsmod.plugins.content.areas.spawns
+
+spawn_npc(npc = Npcs.DWARF_118, x = 2873, z = 9879, walkRadius = 5, direction = Direction.EAST)
+spawn_npc(npc = Npcs.DWARF_118, x = 2868, z = 9882, walkRadius = 5, direction = Direction.WEST)
+spawn_npc(npc = Npcs.DWARF_118, x = 2862, z = 9882, walkRadius = 5, direction = Direction.WEST)
+spawn_npc(npc = Npcs.DWARF_118, x = 2855, z = 9882, walkRadius = 5, direction = Direction.WEST)
+spawn_npc(npc = Npcs.DWARF_118, x = 2847, z = 9879, walkRadius = 5, direction = Direction.SOUTH_WEST)
+spawn_npc(npc = Npcs.DWARF_118, x = 2847, z = 9874, walkRadius = 5, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.DWARF_118, x = 2846, z = 9869, walkRadius = 5, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.DWARF_118, x = 2840, z = 9864, walkRadius = 5, direction = Direction.SOUTH_WEST)
+spawn_npc(npc = Npcs.DWARF_118, x = 2831, z = 9865, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.DWARF_118, x = 2826, z = 9873, walkRadius = 5, direction = Direction.NORTH_WEST)
+
+spawn_npc(npc = Npcs.CHAOS_DWARF, x = 2862, z = 9877, walkRadius = 5, direction = Direction.EAST)
+spawn_npc(npc = Npcs.CHAOS_DWARF, x = 2866, z = 9877, walkRadius = 5, direction = Direction.EAST)
+spawn_npc(npc = Npcs.CHAOS_DWARF, x = 2870, z = 9877, walkRadius = 5, direction = Direction.EAST)
+spawn_npc(npc = Npcs.CHAOS_DWARF, x = 2867, z = 9874, walkRadius = 5, direction = Direction.EAST)
+spawn_npc(npc = Npcs.CHAOS_DWARF, x = 2863, z = 9874, walkRadius = 5, direction = Direction.WEST)
