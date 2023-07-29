0d32fc85ada78a0ee35d1e56f54fabfa92d2d57b
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2023-07-29 19:47:15 +0200

    Fixed random event logout coords

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/randoms/random_event_handler.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/randoms/random_event_handler.plugin.kts
@@ -76,7 +76,7 @@ on_timer(ANTI_CHEAT_TIMER) {
 on_logout {
     if(player.tile.regionId == 12619 || player.attr[ANTI_CHEAT_EVENT_ACTIVE] == true) {
         player.timers.remove(LOGOUT_TIMER)
-        player.moveTo(3222, 3222, 0)
+        player.moveTo(2606, 3104, 0)
         player.attr[ANTI_CHEAT_EVENT_ACTIVE] = false
         player.attr[BOTTING_SCORE] = (player.attr[BOTTING_SCORE] ?: 0) + 1
     }
