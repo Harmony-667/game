d523c662e8db5afaa04eaed4686e829d2853a0de
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2023-07-30 14:04:27 +0200

    Add Seers Spawns

---
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/spawns/seers.plugin.kts
@@ -0,0 +1,53 @@
+package gg.rsmod.plugins.content.areas.spawns
+
+spawn_npc(npc = Npcs.ROGUE, x = 2681, z = 3487, walkRadius = 5, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.ROGUE, x = 2685, z = 3487, walkRadius = 5, direction = Direction.EAST)
+spawn_npc(npc = Npcs.ROGUE, x = 2683, z = 3485, walkRadius = 5, direction = Direction.SOUTH_WEST)
+
+spawn_npc(npc = Npcs.VILLAGER, x = 2703, z = 3475, walkRadius = 5, direction = Direction.SOUTH_EAST)
+spawn_npc(npc = Npcs.VILLAGER_1891, x = 2701, z = 3472, walkRadius = 5, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.VILLAGER_1895, x = 2700, z = 3475, walkRadius = 5, direction = Direction.NORTH_WEST)
+
+spawn_npc(npc = Npcs.HERO, x = 2737, z = 3474, walkRadius = 5, direction = Direction.EAST)
+spawn_npc(npc = Npcs.HERO, x = 2734, z = 3474, walkRadius = 5, direction = Direction.WEST)
+spawn_npc(npc = Npcs.HERO, x = 2737, z = 3467, walkRadius = 5, direction = Direction.WEST)
+spawn_npc(npc = Npcs.HERO, x = 2733, z = 3468, walkRadius = 5, direction = Direction.NORTH_WEST)
+
+spawn_npc(npc = Npcs.CHICKEN, x = 2748, z = 3462, walkRadius = 5, direction = Direction.SOUTH_WEST)
+spawn_npc(npc = Npcs.CHICKEN, x = 2750, z = 3460, walkRadius = 5, direction = Direction.SOUTH_EAST)
+spawn_npc(npc = Npcs.CHICKEN, x = 2747, z = 3460, walkRadius = 5, direction = Direction.WEST)
+spawn_npc(npc = Npcs.CHICKEN, x = 2744, z = 3459, walkRadius = 5, direction = Direction.SOUTH_WEST)
+spawn_npc(npc = Npcs.CHICKEN, x = 2746, z = 3458, walkRadius = 5, direction = Direction.SOUTH_EAST)
+spawn_npc(npc = Npcs.CHICKEN, x = 2749, z = 3458, walkRadius = 5, direction = Direction.EAST)
+spawn_npc(npc = Npcs.CHICKEN, x = 2752, z = 3458, walkRadius = 5, direction = Direction.EAST)
+spawn_npc(npc = Npcs.CHICKEN, x = 2750, z = 3456, walkRadius = 5, direction = Direction.SOUTH_WEST)
+spawn_npc(npc = Npcs.CHICKEN, x = 2746, z = 3456, walkRadius = 5, direction = Direction.WEST)
+
+spawn_npc(npc = Npcs.UNICORN, x = 2716, z = 3509, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.UNICORN, x = 2709, z = 3508, walkRadius = 5, direction = Direction.NORTH_WEST)
+
+spawn_npc(npc = Npcs.DWARF_118, x = 2691, z = 3506, walkRadius = 5, direction = Direction.WEST)
+spawn_npc(npc = Npcs.DWARF_118, x = 2694, z = 3506, walkRadius = 5, direction = Direction.EAST)
+spawn_npc(npc = Npcs.DWARF_118, x = 2698, z = 3505, walkRadius = 5, direction = Direction.SOUTH_EAST)
+spawn_npc(npc = Npcs.DWARF_118, x = 2695, z = 3503, walkRadius = 5, direction = Direction.SOUTH_WEST)
+spawn_npc(npc = Npcs.DWARF_118, x = 2692, z = 3503, walkRadius = 5, direction = Direction.WEST)
+
+spawn_npc(npc = Npcs.IMP_709, x = 2722, z = 3473, walkRadius = 5, direction = Direction.SOUTH_WEST)
+
+spawn_npc(npc = Npcs.TANNER_2320, x = 2712, z = 3478, walkRadius = 5, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.MASTER_CRAFTER, x = 2713, z = 3471, walkRadius = 5, direction = Direction.SOUTH_WEST)
+spawn_npc(npc = Npcs.ROMMIK, x = 2714, z = 3480, walkRadius = 5, direction = Direction.NORTH)
+
+spawn_npc(npc = Npcs.BANKER_CLASSIC_MALE_PURPLE_44, x = 2729, z = 3495, walkRadius = 5, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.BANKER_CLASSIC_MALE_PURPLE_44, x = 2728, z = 3495, walkRadius = 5, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.BANKER_CLASSIC_FEMALE_PURPLE_45, x = 2727, z = 3495, walkRadius = 5, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.BANKER_CLASSIC_FEMALE_PURPLE_45, x = 2724, z = 3495, walkRadius = 5, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.BANKER_CLASSIC_MALE_PURPLE_44, x = 2722, z = 3495, walkRadius = 5, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.BANKER_CLASSIC_MALE_PURPLE_44, x = 2721, z = 3495, walkRadius = 5, direction = Direction.SOUTH)
+
+spawn_npc(npc = Npcs.HIGH_PRIEST, x = 2695, z = 3463, walkRadius = 5, direction = Direction.WEST)
+spawn_npc(npc = Npcs.MONK, x = 2698, z = 3461, walkRadius = 5, direction = Direction.EAST)
+spawn_npc(npc = Npcs.MONK, x = 2698, z = 3465, walkRadius = 5, direction = Direction.NORTH)
+spawn_npc(npc = Npcs.MONK, x = 2700, z = 3463, walkRadius = 5, direction = Direction.SOUTH_EAST)
+
+spawn_npc(npc = Npcs.MYSTERIOUS_MAGE, x = 2739, z = 3481, walkRadius = 5, direction = Direction.SOUTH)
