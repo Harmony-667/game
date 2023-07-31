02c3717a492b9b836972904ba56f8385170dfa1f
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2023-07-31 21:28:43 +0200

    Add Fishing Guild Spawns

---
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/spawns/fishingguild.plugin.kts
@@ -0,0 +1,37 @@
+package gg.rsmod.plugins.content.areas.spawns
+/** Ground items */
+spawn_item(item = Items.HARPOON, amount = 1, x = 2613, z = 3399, respawnCycles = 300)
+spawn_item(item = Items.FISHING_ROD, amount = 1, x = 2607, z = 3394, respawnCycles = 300)
+spawn_item(item = Items.FLY_FISHING_ROD, amount = 1, x = 2606, z = 3394, respawnCycles = 300)
+spawn_item(item = Items.LOBSTER_POT, amount = 1, x = 2605, z = 3401, respawnCycles = 300)
+spawn_item(item = Items.BIG_FISHING_NET, amount = 1, x = 2611, z = 3395, respawnCycles = 300)
+spawn_item(item = Items.SMALL_FISHING_NET, amount = 1, x = 2604, z = 3401, respawnCycles = 300)
+
+/** Fishing Spots */
+spawn_npc(npc = Npcs.FISHING_SPOT_CAGE_HARPOON, x = 2598, z = 3422, walkRadius = 0, direction = Direction.NORTH)
+spawn_npc(npc = Npcs.FISHING_SPOT_CAGE_HARPOON, x = 2598, z = 3419, walkRadius = 0, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.FISHING_SPOT_952, x = 2602, z = 3419, walkRadius = 0, direction = Direction.EAST)
+spawn_npc(npc = Npcs.FISHING_SPOT_952, x = 2602, z = 3422, walkRadius = 0, direction = Direction.NORTH)
+spawn_npc(npc = Npcs.FISHING_SPOT_334, x = 2603, z = 3426, walkRadius = 0, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.FISHING_SPOT_334, x = 2605, z = 3424, walkRadius = 0, direction = Direction.SOUTH_EAST)
+spawn_npc(npc = Npcs.FISHING_SPOT_NET_HARPOON, x = 2605, z = 3420, walkRadius = 0, direction = Direction.WEST)
+spawn_npc(npc = Npcs.FISHING_SPOT_NET_HARPOON, x = 2601, z = 3423, walkRadius = 0, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.FISHING_SPOT_334, x = 2612, z = 3414, walkRadius = 0, direction = Direction.SOUTH_EAST)
+spawn_npc(npc = Npcs.FISHING_SPOT_334, x = 2611, z = 3416, walkRadius = 0, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.FISHING_SPOT_NET_HARPOON, x = 2604, z = 3417, walkRadius = 0, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.FISHING_SPOT_NET_HARPOON, x = 2607, z = 3416, walkRadius = 0, direction = Direction.SOUTH_EAST)
+spawn_npc(npc = Npcs.FISHING_SPOT_952, x = 2608, z = 3413, walkRadius = 0, direction = Direction.SOUTH_EAST)
+spawn_npc(npc = Npcs.FISHING_SPOT_952, x = 2605, z = 3413, walkRadius = 0, direction = Direction.WEST)
+spawn_npc(npc = Npcs.FISHING_SPOT_CAGE_HARPOON, x = 2602, z = 3413, walkRadius = 0, direction = Direction.WEST)
+spawn_npc(npc = Npcs.FISHING_SPOT_CAGE_HARPOON, x = 2605, z = 3410, walkRadius = 0, direction = Direction.SOUTH_EAST)
+
+/** NPC's */
+spawn_npc(npc = Npcs.BANKER_LATEST_MALE_HANDSFRONT_4456, x = 2583, z = 3423, walkRadius = 0, direction = Direction.EAST)
+spawn_npc(npc = Npcs.BANKER_LATEST_FEMALE_HANDSFRONT_4459, x = 2583, z = 3422, walkRadius = 0, direction = Direction.EAST)
+spawn_npc(npc = Npcs.ROACHEY, x = 2588, z = 3405, walkRadius = 5, direction = Direction.EAST)
+
+spawn_npc(npc = Npcs.MASTER_FISHER, x = 2606, z = 3397, walkRadius = 5, direction = Direction.WEST)
+
+spawn_npc(npc = Npcs.NED_4475, x = 2600, z = 3425, walkRadius = 2, direction = Direction.NORTH_EAST)
+
+spawn_npc(npc = Npcs.MYSTERIOUS_MAGE, x = 2595, z = 3398, walkRadius = 3, direction = Direction.WEST)
