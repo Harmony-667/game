87ac8f747f7e953adef37469e9d0ef559ca243f6
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2023-08-09 18:19:01 +0200

    Add knife to yanille general store and add store index

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/yanille/yanille_general_store.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/yanille/yanille_general_store.plugin.kts
@@ -5,18 +5,21 @@ import gg.rsmod.plugins.content.mechanics.shops.CoinCurrency
 val shopkeepers = arrayOf(Npcs.SHOPKEEPER, Npcs.SHOP_ASSISTANT)
 
 create_shop("Yanille General Store", CoinCurrency()) {
+    var index = 0
     sampleItems[0] = ShopItem(Items.TINDERBOX_590, 1, resupplyCycles = 5000)
     sampleItems[1] = ShopItem(Items.HAMMER, 1, resupplyCycles = 5000)
     sampleItems[2] = ShopItem(Items.BRONZE_PICKAXE, 1, resupplyCycles = 5000)
     sampleItems[3] = ShopItem(Items.CHISEL, 1, resupplyCycles = 1000)
     sampleItems[4] = ShopItem(Items.BRONZE_HATCHET, 1, resupplyCycles = 1000)
-    items[0] = ShopItem(Items.BRONZE_HATCHET, 5)
-    items[1] = ShopItem(Items.SHEARS, 5)
-    items[2] = ShopItem(Items.BUCKET, 5)
-    items[3] = ShopItem(Items.TINDERBOX_590, 5)
-    items[4] = ShopItem(Items.CHISEL, 5)
-    items[5] = ShopItem(Items.HAMMER, 5)
-    items[6] = ShopItem(Items.BRONZE_PICKAXE, 5)
+    sampleItems[5] = ShopItem(Items.KNIFE, 1, resupplyCycles = 1000)
+    items[index++] = ShopItem(Items.BRONZE_HATCHET, 5)
+    items[index++] = ShopItem(Items.SHEARS, 5)
+    items[index++] = ShopItem(Items.BUCKET, 5)
+    items[index++] = ShopItem(Items.TINDERBOX_590, 5)
+    items[index++] = ShopItem(Items.CHISEL, 5)
+    items[index++] = ShopItem(Items.KNIFE, 5)
+    items[index++] = ShopItem(Items.HAMMER, 5)
+    items[index] = ShopItem(Items.BRONZE_PICKAXE, 5)
 
 }
 
